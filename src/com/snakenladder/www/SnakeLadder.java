00808ea46067dc956e9e5ee68c24b893d08de33f
soubansukri <souban.sukri@gmail.com>
2022-05-24 06:51:41 +0530

    Added:Snake and ladder prog and initial player position

---
+++ src/com/snakenladder/www/SnakeLadder.java
@@ -0,0 +1,11 @@
+package com.snakenladder.www;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		
+		int player1StartPosition=0;
+		System.out.println(" Player 1's initial position is: " +player1StartPosition);
+	}
+
+}
